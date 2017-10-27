4a880f1420098041b80ff6a40cc1c4523389d9a9
Nicolas Modrzyk <hellonico@gmail.com>
2017-10-27 13:48:01 +0900

    update template to 0.0.5

--- src/leiningen/new/jvm_opencv/project.clj
+++ src/leiningen/new/jvm_opencv/project.clj
@@ -3,5 +3,9 @@
   :main HelloCv
   :repositories [["vendredi" "http://hellonico.info:8081/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [opencv/opencv "3.3.0-rc"]
-                 [opencv/opencv-native "3.3.0-rc"]])
+                 [opencv/opencv "3.3.1"]
+                 [opencv/opencv-native "3.3.1"]
+                 ; [opencv/opencv-native "3.3.1" :classifier "osx"]
+                 ; [opencv/opencv-native "3.3.1" :classifier "windows"]
+                 ; [opencv/opencv-native "3.3.1" :classifier "linux"]
+                 ])
