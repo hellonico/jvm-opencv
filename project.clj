51a039ff25dda5db062cd0e59c83c96d6876623b
Nicolas Modrzyk <hellonico@gmail.com>
2017-11-21 15:23:29 +0900

    0.0.8: auto plugin

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jvm-opencv/lein-template "0.0.5"
+(defproject jvm-opencv/lein-template "0.0.8"
   :description "Created with lein-create-template"
   :deploy-repositories [["releases" :clojars]]
   :url "http://example.com/FIXME"
--- src/leiningen/new/jvm_opencv/project.clj
+++ src/leiningen/new/jvm_opencv/project.clj
@@ -1,7 +1,9 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
   :java-source-paths ["java"]
   :main HelloCv
-  :repositories [["vendredi" "http://hellonico.info:8081/repository/hellonico/"]]
+  :plugins [[lein-auto "0.1.3"]]
+  :auto {:default {:file-pattern #"\.(java)$"}}
+  :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [opencv/opencv "3.3.1"]
                  [opencv/opencv-native "3.3.1"]
