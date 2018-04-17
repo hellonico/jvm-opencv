2b9d81371c1335d68084bc8091f218b495f7f8e3
Nicolas Modrzyk <nico@karabiner-software.com>
2018-04-17 10:37:09 +0900

    update native opencv dep for linux

--- src/leiningen/new/jvm_opencv/project.clj
+++ src/leiningen/new/jvm_opencv/project.clj
@@ -6,7 +6,7 @@
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [opencv/opencv "3.3.1"]
-                 [opencv/opencv-native "3.3.1"]
+                 [opencv/opencv-native "3.3.1_4"]
                  ; [opencv/opencv-native "3.3.1" :classifier "osx"]
                  ; [opencv/opencv-native "3.3.1" :classifier "windows"]
                  ; [opencv/opencv-native "3.3.1" :classifier "linux"]
