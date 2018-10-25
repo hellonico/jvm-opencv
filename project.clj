2c72b5a0660e62a2459a4770e11b3793d0dc01f9
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-25 14:30:51 +0900

    new release steps

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
-(defproject jvm-opencv/lein-template "0.0.12-SNAPSHOT"
+(defproject jvm-opencv/lein-template "0.0.12"
   :description "Created with lein-create-template"
   :deploy-repositories [["releases" :clojars]]
   :url "http://example.com/FIXME"
+  :release-tasks [["vcs" "assert-committed"]
+                ["change" "version" "leiningen.release/bump-version" "release"]
+                ["vcs" "commit"]
+                ["vcs" "tag" "--no-sign"]
+                ["deploy" "clojars"]
+                ["change" "version" "leiningen.release/bump-version"]
+                ["vcs" "commit"]
+                ["vcs" "push"]
+                ]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
