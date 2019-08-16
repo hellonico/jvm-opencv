fd819261eb5b4984b68628d17d03273776714917
Nicolas Modrzyk <hellonico@gmail.com>
2019-08-16 11:33:25 +0900

    Version 0.0.29-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jvm-opencv/lein-template "0.0.28"
+(defproject jvm-opencv/lein-template "0.0.29-SNAPSHOT"
   :description "Created with lein-create-template"
   :deploy-repositories [["releases" :clojars]]
   :url "http://example.com/FIXME"
