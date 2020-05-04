be304f083de1707cbccbc52446af2886795d605b
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-04 10:53:38 +0900

    4.3.0-7

--- src/leiningen/new/jvm_opencv.clj
+++ src/leiningen/new/jvm_opencv.clj
@@ -7,7 +7,7 @@
 (defn jvm-opencv
   [name]
   (let [data {:name name
-              :version "4.3.0-1"
+              :version "4.3.0-7"
               :ns-name (sanitize-ns name)
               :sanitized (name-to-path name)}]
     (->files data 
